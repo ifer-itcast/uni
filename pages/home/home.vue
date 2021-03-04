da0ffc92a23dd9178bd24aa6e4075c82acc761bf
ifer-itcast <yangkangkang@itcast.cn>
2021-03-04 21:39:59 +0800

    点击楼层图片跳转到商品列表

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -23,14 +23,14 @@
         <!-- 楼层图片区域 -->
         <view class="floor-img-box">
           <!-- 左侧大图片的盒子 -->
-          <view class="left-img-box">
+          <navigator class="left-img-box" :url="item.product_list[0].url">
             <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" mode="widthFix"></image>
-          </view>
+          </navigator>
           <!-- 右侧 4 个小图片的盒子 -->
           <view class="right-img-box">
-            <view class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0">
+            <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
               <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
-            </view>
+            </navigator>
           </view>
         </view>
       </view>
@@ -77,6 +77,11 @@
       async getFloorList() {
         const { data: res } = await uni.$http.get('/api/public/v1/home/floordata')
         if (res.meta.status !== 200) return uni.$showMsg()
+        res.message.forEach(floor => {
+          floor.product_list.forEach(prod => {
+            prod.url = '/subpkg/goods_list/goods_list?'+prod.navigator_url.split('?')[1]
+          })
+        })
         this.floorList = res.message
       },
     }
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,6 @@
 <template>
 	<view>
-		xxxddddddd
+		xx
 	</view>
 </template>
 
