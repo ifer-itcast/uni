06bac17a7c09820557460b15e8019e2c5f59f349
ifer-itcast <yangkangkang@itcast.cn>
2021-03-04 19:48:24 +0800

    配置网络请求

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
 import App from './App'
 
+import {} from '@escook/request-miniprogram'
+
+uni.$http = $http
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+  uni.showLoading({
+    title: '数据加载中...'
+  })
+}
+
+// 响应拦截器
+$http.afterRequest = function() {
+  uni.hideLoading()
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
