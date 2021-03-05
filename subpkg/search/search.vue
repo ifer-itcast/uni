51c0d670cad3ff17403c14c7c5b49dd575485b6f
ifer-itcast <yangkangkang@itcast.cn>
2021-03-05 22:29:27 +0800

    搜索框自动获取焦点和防抖

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -9,13 +9,21 @@
 	export default {
 		data() {
 			return {
-				
+				// 延时器的 timerId
+        timer: null,
+        // 搜索关键词
+        kw: ''
 			};
 		},
     methods: {
       input(e) {
-        // e 是最新的搜索内容
-        console.log(e)
+        clearTimeout(this.timer)
+          // 重新启动一个延时器，并把 timerId 赋值给 this.timer
+          this.timer = setTimeout(() => {
+            // 如果 500 毫秒内，没有触发新的输入事件，则为搜索关键词赋值
+            this.kw = e
+            console.log(this.kw)
+          }, 500)
       }
     }
 	}
