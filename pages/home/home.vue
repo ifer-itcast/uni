be25a16b2a05515632d774000df5c6b4affd2006
ifer-itcast <yangkangkang@itcast.cn>
2021-03-05 22:12:01 +0800

    实现首页搜索组件的吸顶效果

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,8 @@
 <template>
 	<view>
+    <view class="search-box">
+      <my-search @click="gotoSearch"></my-search>
+    </view>
     <!-- 小圆点、自动轮播、间隔、每张轮播耗时、无缝滚动 -->
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
       <swiper-item v-for="(item, i) in swiperList" :key="i">
@@ -84,6 +87,11 @@
         })
         this.floorList = res.message
       },
+      gotoSearch() {
+        uni.navigateTo({
+          url: '/subpkg/search/search'
+        })
+      }
     }
 	}
 </script>
@@ -122,5 +130,14 @@ swiper {
 .floor-img-box {
   display: flex;
   padding-left: 10rpx;
+}
+
+.search-box {
+  // 设置定位效果为“吸顶”
+  position: sticky;
+  // 吸顶的“位置”
+  top: 0;
+  // 提高层级，防止被轮播图覆盖
+  z-index: 999;
 }
 </style>
