9dfe48802cb6c9c3df503a3f63e2cb275605c4b0
ifer-itcast <yangkangkang@itcast.cn>
2021-03-05 22:23:18 +0800

    渲染搜索页面的基本结构

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,6 +1,7 @@
 <template>
-	<view>
-		Search
+	<view class="search-box">
+	  <!-- 使用 uni-ui 提供的搜索组件 -->
+	  <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
 	</view>
 </template>
 
@@ -10,10 +11,20 @@
 			return {
 				
 			};
-		}
+		},
+    methods: {
+      input(e) {
+        // e 是最新的搜索内容
+        console.log(e)
+      }
+    }
 	}
 </script>
 
 <style lang="scss">
-
+.search-box {
+  position: sticky;
+  top: 0;
+  z-index: 999;
+}
 </style>
