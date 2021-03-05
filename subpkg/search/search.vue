2de57a8d85fad6c4efb17b135c375322e8a0329b
ifer-itcast <yangkangkang@itcast.cn>
2021-03-05 22:40:26 +0800

    渲染搜索建议点击并跳转到商品详情

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -2,6 +2,12 @@
 	<view class="search-box">
 	  <!-- 使用 uni-ui 提供的搜索组件 -->
 	  <uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
+    <view class="sugg-list">
+      <view class="sugg-item" v-for="(item, i) in searchResults" :key="i" @click="gotoDetail(item.goods_id)">
+        <view class="goods-name">{{item.goods_name}}</view>
+        <uni-icons type="arrowright" size="16"></uni-icons>
+      </view>
+    </view>
 	</view>
 </template>
 
@@ -36,6 +42,12 @@
         const { data: res } = await uni.$http.get('/api/public/v1/goods/qsearch', { query: this.kw })
         if (res.meta.status !== 200) return uni.$showMsg()
         this.searchResults = res.message
+      },
+      gotoDetail(goods_id) {
+        uni.navigateTo({
+          // 指定详情页面的 URL 地址，并传递 goods_id 参数
+          url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods_id
+        })
       }
     }
 	}
@@ -46,5 +58,27 @@
   position: sticky;
   top: 0;
   z-index: 999;
+}
+.sugg-list {
+  padding: 0 5px;
+
+  .sugg-item {
+    font-size: 12px;
+    padding: 13px 0;
+    border-bottom: 1px solid #efefef;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .goods-name {
+      // 文字不允许换行（单行文本）
+      white-space: nowrap;
+      // 溢出部分隐藏
+      overflow: hidden;
+      // 文本溢出后，使用 ... 代替
+      text-overflow: ellipsis;
+      margin-right: 3px;
+    }
+  }
 }
 </style>
