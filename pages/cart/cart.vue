68d6fa80f48b817efd759e239d961170c0e77cc6
ifer-itcast <yangkangkang@itcast.cn>
2021-03-06 23:41:53 +0800

    动态为tabBar设置数字徽标

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -5,13 +5,30 @@
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+import { mapGetters } from 'vuex'
+
+export default {
+  data() {
+    return {}
+  },
+  computed: {
+    // 将 m_cart 模块中的 total 映射为当前页面的计算属性
+    ...mapGetters('m_cart', ['total']),
+  },
+  onShow() {
+     // 在页面刚展示的时候，设置数字徽标
+     this.setBadge()
+  },
+  methods: {
+     setBadge() {
+        // 调用 uni.setTabBarBadge() 方法，为购物车设置右上角的徽标
+        uni.setTabBarBadge({
+           index: 2, // 索引
+           text: this.total + '' // 注意：text 的值必须是字符串，不能是数字
+        })
+     }
+  }
+}
 </script>
 
 <style lang="scss">
