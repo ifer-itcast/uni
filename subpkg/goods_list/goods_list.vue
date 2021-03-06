48fdda910e7320c031432d9103461f428cbb005e
ifer-itcast <yangkangkang@itcast.cn>
2021-03-06 21:41:43 +0800

    把商品Item项封装为自定义组件

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,24 +1,11 @@
 <template>
 	<view>
 		<view class="goods-list">
-		      <block v-for="(goods, i) in goodsList" :key="i">
-		        <view class="goods-item">
-		          <!-- 商品左侧图片区域 -->
-		          <view class="goods-item-left">
-		            <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
-		          </view>
-		          <!-- 商品右侧信息区域 -->
-		          <view class="goods-item-right">
-		            <!-- 商品标题 -->
-		            <view class="goods-name">{{goods.goods_name}}</view>
-		            <view class="goods-info-box">
-		              <!-- 商品价格 -->
-		              <view class="goods-price">￥{{goods.goods_price}}</view>
-		            </view>
-		          </view>
-		        </view>
-		      </block>
-		    </view>
+		  <block v-for="(item, i) in goodsList" :key="i">
+		    <!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+		    <my-goods :goods="item"></my-goods>
+		  </block>
+		</view>
 	</view>
 </template>
 
@@ -40,7 +27,6 @@
         goodsList: [],
         // 总数量，用来实现分页
         total: 0,
-        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
 			};
 		},
     onLoad(options) {
@@ -61,36 +47,3 @@
     }
 	}
 </script>
-
-<style lang="scss">
-.goods-item {
-  display: flex;
-  padding: 10px 5px;
-  border-bottom: 1px solid #f0f0f0;
-
-  .goods-item-left {
-    margin-right: 5px;
-
-    .goods-pic {
-      width: 100px;
-      height: 100px;
-      display: block;
-    }
-  }
-
-  .goods-item-right {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-
-    .goods-name {
-      font-size: 13px;
-    }
-
-    .goods-price {
-      font-size: 16px;
-      color: #c00000;
-    }
-  }
-}
-</style>
