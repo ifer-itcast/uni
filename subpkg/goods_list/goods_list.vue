3ef816bb0661d761784344391521756898e2afdb
ifer-itcast <yangkangkang@itcast.cn>
2021-03-06 21:28:10 +0800

    获取商品列表数据

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -8,9 +8,38 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+          // 查询关键词
+          query: '',
+          // 商品分类Id
+          cid: '',
+          // 页码值
+          pagenum: 1,
+          // 每页显示多少条数据
+          pagesize: 10
+        },
+        // 商品列表的数据
+        goodsList: [],
+        // 总数量，用来实现分页
+        total: 0
 			};
-		}
+		},
+    onLoad(options) {
+      // 将页面参数转存到 this.queryObj 对象中
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      this.getGoodsList()
+    },
+    methods: {
+      async getGoodsList() {
+        // 发起请求
+        const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 为数据赋值
+        this.goodsList = res.message.goods
+        this.total = res.message.total
+      }
+    }
 	}
 </script>
 
