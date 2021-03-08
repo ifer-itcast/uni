4c8131ede54d5b1f99e7881ac6570b46fc2f1efa
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 17:34:48 +0800

    将用户信息存储到Vuex

--- components/my-login/my-login.vue
+++ components/my-login/my-login.vue
@@ -11,64 +11,70 @@
 </template>
 
 <script>
-	export default {
-		name:"my-login",
-		data() {
-			return {
-				
-			};
-		},
+  import {
+    mapMutations
+  } from 'vuex'
+  export default {
+    name: "my-login",
+    data() {
+      return {
+
+      };
+    },
     methods: {
+      // 2. 调用 mapMutations 辅助方法，把 m_user 模块中的 updateUserInfo 映射到当前组件中使用
+      ...mapMutations('m_user', ['updateUserInfo']),
       // 获取微信用户的基本信息
       getUserInfo(e) {
         // 判断是否获取用户信息成功
         if (e.detail.errMsg === 'getUserInfo:fail auth deny') return uni.$showMsg('您取消了登录授权！')
-    
+
         // 获取用户信息成功， e.detail.userInfo 就是用户的基本信息
-        console.log(e.detail.userInfo)
+        // 3. 将用户的基本信息存储到 vuex 中
+        this.updateUserInfo(e.detail.userInfo)
       }
     }
-	}
+  }
 </script>
 
 <style lang="scss">
-.login-container {
-  // 登录盒子的样式
-  height: 750rpx;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: #f8f8f8;
-  position: relative;
-  overflow: hidden;
+  .login-container {
+    // 登录盒子的样式
+    height: 750rpx;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #f8f8f8;
+    position: relative;
+    overflow: hidden;
 
-  // 绘制登录盒子底部的半椭圆造型
-  &::after {
-    content: ' ';
-    display: block;
-    position: absolute;
-    width: 100%;
-    height: 40px;
-    left: 0;
-    bottom: 0;
-    background-color: white;
-    border-radius: 100%;
-    transform: translateY(50%);
-  }
+    // 绘制登录盒子底部的半椭圆造型
+    &::after {
+      content: ' ';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 40px;
+      left: 0;
+      bottom: 0;
+      background-color: white;
+      border-radius: 100%;
+      transform: translateY(50%);
+    }
 
-  // 登录按钮的样式
-  .btn-login {
-    width: 90%;
-    border-radius: 100px;
-    margin: 15px 0;
-    background-color: #c00000;
-  }
+    // 登录按钮的样式
+    .btn-login {
+      width: 90%;
+      border-radius: 100px;
+      margin: 15px 0;
+      background-color: #c00000;
+    }
 
-  // 按钮下方提示消息的样式
-  .tips-text {
-    font-size: 12px;
-    color: gray;
+    // 按钮下方提示消息的样式
+    .tips-text {
+      font-size: 12px;
+      color: gray;
+    }
   }
-}
 </style>
--- store/user.js
+++ store/user.js
@@ -4,10 +4,13 @@ export default {
 
   // state 数据
   state: () => ({
-    // 3. 读取本地的收货地址数据，初始化 address 对象
-       address: JSON.parse(uni.getStorageSync('address') || '{}'),
-       // 登录成功之后的 token 字符串
-           token: '',
+    // 收货地址
+    // address: {}
+    address: JSON.parse(uni.getStorageSync('address') || '{}'),
+    // 登录成功之后的 token 字符串
+    token: '',
+    // 用户的基本信息
+    userinfo: JSON.parse(uni.getStorageSync('userinfo') || '{}')
   }),
 
   // 方法
@@ -15,23 +18,34 @@ export default {
     // 更新收货地址
     updateAddress(state, address) {
       state.address = address
-       // 2. 通过 this.commit() 方法，调用 m_user 模块下的 saveAddressToStorage 方法将 address 对象持久化存储到本地
-            this.commit('m_user/saveAddressToStorage')
+      // 2. 通过 this.commit() 方法，调用 m_user 模块下的 saveAddressToStorage 方法将 address 对象持久化存储到本地
+      this.commit('m_user/saveAddressToStorage')
     },
     // 1. 定义将 address 持久化存储到本地 mutations 方法
-        saveAddressToStorage(state) {
-          uni.setStorageSync('address', JSON.stringify(state.address))
-        },
+    saveAddressToStorage(state) {
+      uni.setStorageSync('address', JSON.stringify(state.address))
+    },
+    // 更新用户的基本信息
+    updateUserInfo(state, userinfo) {
+      state.userinfo = userinfo
+      // 通过 this.commit() 方法，调用 m_user 模块下的 saveUserInfoToStorage 方法，将 userinfo 对象持久化存储到本地
+      this.commit('m_user/saveUserInfoToStorage')
+    },
+
+    // 将 userinfo 持久化存储到本地
+    saveUserInfoToStorage(state) {
+      uni.setStorageSync('userinfo', JSON.stringify(state.userinfo))
+    }
   },
 
   // 数据包装器
   getters: {
     // 收货详细地址的计算属性
-      addstr(state) {
-        if (!state.address.provinceName) return ''
-    
-        // 拼接 省，市，区，详细地址 的字符串并返回给用户
-        return state.address.provinceName + state.address.cityName + state.address.countyName + state.address.detailInfo
-      }
+    addstr(state) {
+      if (!state.address.provinceName) return ''
+
+      // 拼接 省，市，区，详细地址 的字符串并返回给用户
+      return state.address.provinceName + state.address.cityName + state.address.countyName + state.address.detailInfo
+    }
   },
 }
