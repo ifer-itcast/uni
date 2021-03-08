f89dbb937b518b4086bb3bd0225560b98b79ed1d
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 11:47:09 +0800

    渲染商品列表区域的基本结构

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,12 +1,23 @@
 <template>
 	<view>
-		Cart
+		<!-- 购物车商品列表的标题区域 -->
+		<view class="cart-title">
+		  <!-- 左侧的图标 -->
+		  <uni-icons type="shop" size="18"></uni-icons>
+		  <!-- 描述文本 -->
+		  <text class="cart-title-text">购物车</text>
+		</view>
+    <!-- 商品列表区域 -->
+    <block v-for="(goods, i) in cart" :key="i">
+      <my-goods :goods="goods"></my-goods>
+    </block
 	</view>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+// 按需导入 mapState 这个辅助函数
+import { mapState } from 'vuex'
 export default {
   data() {
     return {}
@@ -14,6 +25,7 @@ export default {
   computed: {
     // 将 m_cart 模块中的 total 映射为当前页面的计算属性
     ...mapGetters('m_cart', ['total']),
+    ...mapState('m_cart', ['cart']),
   },
   onShow() {
      // 在页面刚展示的时候，设置数字徽标
@@ -32,5 +44,15 @@ export default {
 </script>
 
 <style lang="scss">
-
+.cart-title {
+  height: 40px;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding-left: 5px;
+  border-bottom: 1px solid #efefef;
+  .cart-title-text {
+    margin-left: 10px;
+  }
+}
 </style>
