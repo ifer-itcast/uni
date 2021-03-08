3e0623379c70e2bacc57ab80249f8a89436b8d97
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 15:31:10 +0800

    实现选择收获地址的功能

--- components/my-address/my-address.vue
+++ components/my-address/my-address.vue
@@ -3,14 +3,14 @@
 	
 	  <!-- 选择收货地址的盒子 -->
 	  <view class="address-choose-box" v-if="JSON.stringify(address) === '{}'">
-	    <button type="primary" size="mini" class="btnChooseAddress">请选择收货地址+</button>
+	    <button type="primary" size="mini" class="btnChooseAddress" @click="chooseAddress">请选择收货地址+</button>
 	  </view>
 	
 	  <!-- 渲染收货信息的盒子 -->
 	  <view class="address-info-box" v-else>
 	    <view class="row1">
 	      <view class="row1-left">
-	        <view class="username">收货人：<text>escook</text></view>
+	        <view class="username">收货人：<text>{{address.userName}}</text></view>
 	      </view>
 	      <view class="row1-right">
 	        <view class="phone">电话：<text>138XXXX5555</text></view>
@@ -19,7 +19,7 @@
 	    </view>
 	    <view class="row2">
 	      <view class="row2-left">收货地址：</view>
-	      <view class="row2-right">河北省邯郸市肥乡区xxx 河北省邯郸市肥乡区xxx 河北省邯郸市肥乡区xxx 河北省邯郸市肥乡区xxx </view>
+	      <view class="row2-right">{{addstr}}</view>
 	    </view>
 	  </view>
 	
@@ -35,7 +35,30 @@
 			return {
 				address: {},
 			};
-		}
+		},
+    computed: {
+      // 收货详细地址的计算属性
+      addstr() {
+        if (!this.address.provinceName) return ''
+    
+        // 拼接 省，市，区，详细地址 的字符串并返回给用户
+        return this.address.provinceName + this.address.cityName + this.address.countyName + this.address.detailInfo
+      }
+    },
+    methods: {
+      // 选择收货地址
+      async chooseAddress() {
+        // 1. 调用小程序提供的 chooseAddress() 方法，即可使用选择收货地址的功能
+        //    返回值是一个数组：第 1 项为错误对象；第 2 项为成功之后的收货地址对象
+        const [err, succ] = await uni.chooseAddress().catch(err => err)
+    
+        // 2. 用户成功的选择了收货地址
+        if (err === null && succ.errMsg === 'chooseAddress:ok') {
+          // 为 data 里面的收货地址对象赋值
+          this.address = succ
+        }
+      }
+    }
 	}
 </script>
 
