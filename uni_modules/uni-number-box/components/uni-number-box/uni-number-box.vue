0022d27f3e2633c4a4291e92878d8ca82f00a9f8
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 12:50:01 +0800

    完善NumberBox的inputValue侦听器

--- uni_modules/uni-number-box/components/uni-number-box/uni-number-box.vue
+++ uni_modules/uni-number-box/components/uni-number-box/uni-number-box.vue
@@ -56,10 +56,16 @@
 				this.inputValue = +val;
 			},
 			inputValue(newVal, oldVal) {
-				if (+newVal !== +oldVal) {
-					this.$emit("change", newVal);
-					this.$emit("input", newVal);
-				}
+        // 官方提供的 if 判断条件，在用户每次输入内容时，都会调用 this.$emit("change", newVal)
+				// if (+newVal !== +oldVal) {
+				// 	this.$emit("change", newVal);
+				// 	this.$emit("input", newVal);
+				// }
+          // 新旧内容不同 && 新值内容合法 && 新值中不包含小数点
+          if (+newVal !== +oldVal && Number(newVal) && String(newVal).indexOf('.') === -1) {
+            this.$emit("change", newVal);
+            this.$emit("input", newVal);
+          }
 			}
 		},
 		created() {
