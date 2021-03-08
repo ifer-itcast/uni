2afd3ff93420f4be477bcee3d4927afeed9ce903
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 15:46:00 +0800

    将Store中的address持久话存储到本地

--- store/user.js
+++ store/user.js
@@ -4,8 +4,8 @@ export default {
 
   // state 数据
   state: () => ({
-    // 收货地址
-    address: {},
+    // 3. 读取本地的收货地址数据，初始化 address 对象
+       address: JSON.parse(uni.getStorageSync('address') || '{}'),
   }),
 
   // 方法
@@ -13,7 +13,13 @@ export default {
     // 更新收货地址
     updateAddress(state, address) {
       state.address = address
+       // 2. 通过 this.commit() 方法，调用 m_user 模块下的 saveAddressToStorage 方法将 address 对象持久化存储到本地
+            this.commit('m_user/saveAddressToStorage')
     },
+    // 1. 定义将 address 持久化存储到本地 mutations 方法
+        saveAddressToStorage(state) {
+          uni.setStorageSync('address', JSON.stringify(state.address))
+        },
   },
 
   // 数据包装器
