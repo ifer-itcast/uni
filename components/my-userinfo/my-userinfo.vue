5b070004dab6a9434d170a8cef27eb771f391ce2
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 18:11:22 +0800

    渲染用户头像和昵称

--- components/my-userinfo/my-userinfo.vue
+++ components/my-userinfo/my-userinfo.vue
@@ -1,20 +1,62 @@
 <template>
-	<view>
-		userinfo
-	</view>
+  <view class="my-userinfo-container">
+
+    <!-- 头像昵称区域 -->
+    <view class="top-box">
+      <image :src="userinfo.avatarUrl" class="avatar"></image>
+      <view class="nickname">{{userinfo.nickName}}</view>
+    </view>
+
+  </view>
 </template>
 
 <script>
-	export default {
-		name:"my-userinfo",
-		data() {
-			return {
-				
-			};
-		}
-	}
+  import {
+    mapState
+  } from 'vuex'
+  export default {
+    name: "my-userinfo",
+    computed: {
+      // 将 m_user 模块中的 userinfo 映射到当前页面中使用
+      ...mapState('m_user', ['userinfo']),
+    },
+    data() {
+      return {
+
+      };
+    }
+  }
 </script>
 
 <style lang="scss">
+  .my-userinfo-container {
+    height: 100%;
+    // 为整个组件的结构添加浅灰色的背景
+    background-color: #f4f4f4;
 
+    .top-box {
+      height: 400rpx;
+      background-color: #c00000;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+
+      .avatar {
+        display: block;
+        width: 90px;
+        height: 90px;
+        border-radius: 45px;
+        border: 2px solid white;
+        box-shadow: 0 1px 5px black;
+      }
+
+      .nickname {
+        color: white;
+        font-weight: bold;
+        font-size: 16px;
+        margin-top: 10px;
+      }
+    }
+  }
 </style>
