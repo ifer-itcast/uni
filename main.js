1dbe9327ee74d626bdd9475371ea3f0976c1578d
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 19:11:45 +0800

    在请求头中添加TOKEN身份认证的字段

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import store from './store/store.js'
-import { $http } from '@escook/request-miniprogram'
+import {
+  $http
+} from '@escook/request-miniprogram'
 
 uni.$http = $http
 
@@ -14,6 +16,14 @@ $http.beforeRequest = function(options) {
   uni.showLoading({
     title: '数据加载中...'
   })
+  // 判断请求的是否为有权限的 API 接口
+  if (options.url.indexOf('/my/') !== -1) {
+    // 为请求头添加身份认证字段
+    options.header = {
+      // 字段的值可以直接从 vuex 中进行获取
+      Authorization: store.state.m_user.token,
+    }
+  }
 }
 
 // 响应拦截器
@@ -22,7 +32,7 @@ $http.afterRequest = function() {
 }
 
 // 弹框
-uni.$showMsg = function(title='数据请求失败', duration=1500) {
+uni.$showMsg = function(title = '数据请求失败', duration = 1500) {
   uni.showToast({
     title,
     duration,
@@ -35,7 +45,7 @@ Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App,
-    store
+  ...App,
+  store
 })
 app.$mount()
