e21c5ebaedf9e63ca8724d05e8ba990087bfb471
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 17:29:59 +0800

    实现登录和用户信息组件的按需展示

---
+++ components/my-login/my-login.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		login
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-login",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
