eb02b6a210a14bd2d600b9f1aa18655d9ea5bf56
ifer-itcast <yangkangkang@itcast.cn>
2021-03-08 11:41:26 +0800

    渲染购物车商品列表的标题区域

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -5,29 +5,14 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import badgeMix from '@/mixins/tabbar-badge.js'
 
 export default {
   data() {
     return {}
   },
-  computed: {
-    // 将 m_cart 模块中的 total 映射为当前页面的计算属性
-    ...mapGetters('m_cart', ['total']),
-  },
-  onShow() {
-     // 在页面刚展示的时候，设置数字徽标
-     this.setBadge()
-  },
-  methods: {
-     setBadge() {
-        // 调用 uni.setTabBarBadge() 方法，为购物车设置右上角的徽标
-        uni.setTabBarBadge({
-           index: 2, // 索引
-           text: this.total + '' // 注意：text 的值必须是字符串，不能是数字
-        })
-     }
-  }
+  // 将 badgeMix 混入到当前的页面中进行使用
+  mixins: [badgeMix],
 }
 </script>
 
